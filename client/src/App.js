eb605a865154106cf2a4cb9ef891f7f29de133e7
Robert Gordon <Komeuppance@gmail.com>
2019-12-05 13:01:10 -0800

    Added styled components and animations

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,15 @@ import {useFetchData} from './hooks/useFetchData'
 
 import PlayerCard from './components/PlayerCard'
 
+import styled from 'styled-components'
 import './App.css';
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  margin: 0 auto;
+`
 
 function App() {
 
@@ -20,9 +28,9 @@ function App() {
         <h1>Women's World Cup</h1>
         <button onClick={()=>{setShowPlayers(!showPlayers)}}>Show the players</button>
       </header>
-      <div>
-        {!showPlayers ? <></> : <div>{players.map(player =>{return <PlayerCard key={player.id} {...player} />})}</div>}
-      </div>
+      <Cards>
+        {!showPlayers ? <></> : players.map(player =>{return <PlayerCard key={player.id} {...player} />})}
+      </Cards>
     </div>
   );
 }
--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -1,12 +1,40 @@
 import React from 'react'
+import styled, {keyframes} from 'styled-components'
+import {flipInY} from 'react-animations'
+
+const flipAnim = keyframes`${flipInY}`
+
+let test = `1000ms`
+
+const Card = styled.div`
+    animation-duration: ${test};
+    animation-name: ${flipAnim};
+    display: flex;
+    flex-direction: column;
+    margin: 15px;
+    width: 200px;
+    height: 130px;
+    background-color: white;
+    border: 1px solid grey;
+    border-radius: 5px;
+    color: darkgray;
+
+`
 
 const PlayerCard = (props) => {
+
+    test = `${props.id}ms`
+
     return(
-        <div>
+        <>
+        {/* {test = `${props.id}s`}
+        {console.log(test)} */}
+        <Card>
             <p><span>Name:</span> {props.name}</p>
             <p><span>Country:</span> {props.country}</p>
             <p><span>Searches:</span> {props.searches}</p>
-        </div>
+        </Card>
+        </>
     )
 }
 
