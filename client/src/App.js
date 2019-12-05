7a7f1053c4cf707d7acdf9555e2707ec72ae8547
Robert Gordon <Komeuppance@gmail.com>
2019-12-05 12:14:10 -0800

    Pulling in data success

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [players, setPlayers] = useState([])
+
+  // const fetchData = ()=>{
+    useEffect(()=>{
+      fetch('http://localhost:5000/api/players')
+      .then (res => res.json())
+      .then (res => setPlayers(res))
+      .catch (err => {console.log(err)})
+    },[])
+  // }
+
+    console.log(players)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
